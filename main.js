af4380d2f916dbb9037bcd5759c0ecd49b17d238
Patrick <patrick.hurley@codeimmersives.com>
2020-01-30 18:10:13 -0500

    initial commit

--- main.js
+++ main.js
@@ -5,7 +5,39 @@
 
 
 
- 
+function capitalize(anything){
+  return anything.toUpperCase();
+}
+
+function exclaim(important) {
+  return important + '!';
+} 
+const result = exclaim ('hey')
+result;
+
+function firstCharacter(word){
+  return word[0];
+}
+const result1 = firstCharacter ('hello!')
+result1;
+
+function lastCharacter(stuff){
+  return stuff[stuff.length - 1]
+}
+
+const result2 = lastCharacter('hello')
+result2;
+
+function oneCharacter(word, x) {
+  return word[x];
+}
+function twoCharacters(word, x, y){
+  return word[x] + word[y];
+}
+
+const result3 = twoCharacters('hello', 3, 4)
+result3;
+
 
 /********************************************************************
  * THIS CODE IS FOR INTERNAL USE ONLY. DON'T CHANGE ANYTHING BELOW! *
